edb65cf59462ea7abbd5123b433091f011aa7477
Andrew Febrianto <andr3w.febrianto@gmail.com>
2023-08-16 15:38:04 +0700

    fix route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,9 +17,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-/*Route::get('/', function () {
-    return view('welcome');
-});*/
+Route::get('/', function () {
+    if (Auth::hasUser()){
+        redirect()->route('home');
+    }
+
+    return redirect()->route('login');
+});
 
 /*Route::get('/dashboard', function () {
     return view('dashboard');
